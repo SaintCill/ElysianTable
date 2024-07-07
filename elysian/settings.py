299d89b1063bfd546bd82cdce9bbd30837c6701c
SaintCill <153657990+SaintCill@users.noreply.github.com>
2024-07-07 03:58:23 +0200

    Changed database variables again.

--- elysian/settings.py
+++ elysian/settings.py
@@ -79,15 +79,8 @@ WSGI_APPLICATION = 'elysian.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
+
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'elysian_table',
