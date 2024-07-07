a5d1a3e6b9a15b84035f6f1ab636128ada51d6db
SaintCill <153657990+SaintCill@users.noreply.github.com>
2024-07-07 03:24:42 +0200

    Tweaked some more values in settings.py

--- elysian/settings.py
+++ elysian/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'elysian.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 DATABASES = {
     'default': dj_database_url.config('postgresql://elysian_table_user:2byBji7VdikVpx3Hg6HhlHqDPAO8HqW7@dpg-cq4s2bg8fa8c73fugfrg-a/elysian_table', conn_max_age=600)
